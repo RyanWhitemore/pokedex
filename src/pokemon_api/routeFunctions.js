c85b45d7a9301ad2d6fcdf587da39b474eea218b
Ryan Whitemore <ryan.whitemore@gmail.com>
2023-05-17 14:59:21 -0400

    refactored to use connection pool

--- src/pokemon_api/routeFunctions.js
+++ src/pokemon_api/routeFunctions.js
@@ -1,48 +1,49 @@
 const mysql = require('mysql2')
 const dotenv = require('dotenv').config()
 const bcrypt = require('bcrypt')
+const getConnection = require('./pool.js')
 
 const user = process.env.USER
 const pass = process.env.PASSWORD
 const saltRounds = parseInt(process.env.SALTROUNDS)
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    database: 'pokemon_prototype_db',
-    user: user,
-    password: pass
-})
+
 
 const registerUser = async (req, res) => {
     const { username, password } = req.body
-    con.query('SELECT username FROM users WHERE username = ?', [username], async (err, results) => {
-        if (err) {
-            throw(err)
-        }
-        if (results.length > 0) {
-            return res.status(400).send('username taken')
-        } else {
-            const salt = await bcrypt.genSalt(saltRounds)
-            const hash = await bcrypt.hash(password, salt)
-            con.query(`INSERT INTO users (username, password) VALUES (?, ?)`, [username, hash], (err, results) => {
-                if (err) {
-                    throw err
-                }
-            con.query("SELECT user_id FROM users WHERE username = ?", [username], (err, results) => {
-                if (err) {
-                    throw(err)
-                } else {
-                    con.query("INSERT INTO pokemon_users (user_id, pokemon_id) SELECT ?, pokemon_id FROM pokemon", [parseInt(results[0].user_id)], (err, results) => {
-                        if (err) {
-                            throw (err)
-                        }
-                    })
-                }
-            })
-            res.status(201).send(`user with username ${username} created`)
-            })
-        }
+
+    getConnection((err, con) => {
+        con.query('SELECT username FROM users WHERE username = ?', [username], async (err, results) => {
+            if (err) {
+                throw(err)
+            }
+            if (results.length > 0) {
+                return res.status(400).send('username taken')
+            } else {
+                const salt = await bcrypt.genSalt(saltRounds)
+                const hash = await bcrypt.hash(password, salt)
+                con.query(`INSERT INTO users (username, password) VALUES (?, ?)`, [username, hash], (err, results) => {
+                    if (err) {
+                        throw err
+                    }
+                con.query("SELECT user_id FROM users WHERE username = ?", [username], (err, results) => {
+                    if (err) {
+                        throw(err)
+                    } else {
+                        con.query("INSERT INTO pokemon_users (user_id, pokemon_id) SELECT ?, pokemon_id FROM pokemon", [parseInt(results[0].user_id)], (err, results) => {
+                            if (err) {
+                                throw (err)
+                            }
+                        })
+                    }
+                })
+                res.status(201).send(`user with username ${username} created`)
+                })
+            }
+        })
+        con.release()
     })
+    
 }
 
 const returnResults = (res, results) => {
@@ -60,9 +61,9 @@ const returnResults = (res, results) => {
 
 const sort = (req, res, next) => {
 
+    const {area, type, caught, userID, version} = req.query
     
-        const {area, type, caught, userID, version} = req.query
-
+    getConnection((err, con) => {
         con.query(`
             CREATE OR REPLACE VIEW ${userID + version} AS
             SELECT pokemon_users.is_caught,
@@ -119,8 +120,10 @@ const sort = (req, res, next) => {
             } else {
                 returnResults(res, results)
             }
+            con.release()
         })
     }
+    })
     
 }
 
