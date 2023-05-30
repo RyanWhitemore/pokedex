f61e42a8462ecf27390d47c28fcc12e93247b8d4
Ryan Whitemore <ryan.whitemore@gmail.com>
2023-05-30 09:43:44 -0400

    refactored TableRows.jsx:

--- src/TableRows.jsx
+++ src/TableRows.jsx
@@ -1,4 +1,5 @@
 import Image from "./Image"
+import { FixedSizeList as List } from "react-window"
 
 const TableRows = ({handleChange, pokemon}) => {
     const pokemonHtml = []
@@ -24,7 +25,21 @@ const TableRows = ({handleChange, pokemon}) => {
     }
     
 
-    return pokemonHtml
+    return <div className="table-row">
+        {pokemon.map(pokemon => {
+            <tr value={pokemon.pokemon_id} key={pokemon.pokemon_id + 1}>
+                <td><Image name={pokemon.pokemon_name} id={pokemon.pokemon_id} /></td>
+                <td>{pokemon.pokemon_id}</td>
+                <td>{pokemon.pokemon_name}</td>
+                <td>{pokemon.region}</td>
+                <td>{pokemon.type}</td>
+                <td><input type="checkbox"
+                defaultChecked={pokemon.is_caught > 0 ? "checked" : ""}
+                onChange={(e) => handleChange(pokemon.pokemon_id)}
+                /></td>
+            </tr>
+        })}
+    </div>
 }
 
 export default TableRows
